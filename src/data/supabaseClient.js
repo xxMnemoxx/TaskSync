5ecb5532055dbd5d2780e70ceb0495ea91405eae
xxMnemoxx <jayzelmaellanasas@gmail.com>
2026-09-04 02:02:07 +0800

    Prepare for deployment

--- src/data/supabaseClient.js
+++ src/data/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tenuvwcyiyqtrzairydb.supabase.co';
-const supabaseKey = 'sb_publishable_HMI1Qu40qlnHGmxH7PrCJg_GSLn0tjf';
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
